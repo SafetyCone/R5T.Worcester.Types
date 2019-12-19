33031538d6b3110ecec6a218a652fe60daf669f5
MinexAutomation <minex.automation@gmail.com>
2019-12-18 23:10:03 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Worcester.Types.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Worcester.Types.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Worcester.Types/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Worcester.Types
+{
+    public class Class1
+    {
+    }
+}
